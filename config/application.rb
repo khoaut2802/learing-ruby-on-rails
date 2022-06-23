15885d7b54cdbfc2ef7ebbd5a2c3ff453939743b
Khoa Nguyen <khoanx@imt-soft.com>
2022-06-23 08:45:25 +0700

    - Update source code
    - Update source code

--- config/application.rb
+++ config/application.rb
@@ -6,11 +6,14 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module LearningRor
+module SampleApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :vi
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
